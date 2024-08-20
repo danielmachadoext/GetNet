1c36723ea875c67bf350bfb847466c3b95629791
danielmachadoext <daniel87machado@gmail.com>
2024-08-20 13:10:50 -0300

    pageObjects

--- cypress/e2e/get_net_front.cy.js
+++ cypress/e2e/get_net_front.cy.js
@@ -1,5 +1,5 @@
-import HomePage from  '..HomePage';
-import CentralDeAjudaPage from '..CentralDeAjudaPage';
+import HomePage from  '../support/pageObjects/HomePage';
+import CentralDeAjudaPage from '../support/pageObjects/CentralDeAjudaPage';
 
 describe('Teste Automatizado Cypress', () => {
     const homePage = new HomePage();
